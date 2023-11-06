bc2185c4aff42ab16d405fa6e1f243450fa6a192
Ilya Isaev <ilyajoice@gmail.com>
2023-11-07 00:43:55 +0300

    content update

--- menu/templatetags/treemenu_tags.py
+++ menu/templatetags/treemenu_tags.py
@@ -11,25 +11,7 @@ register = template.Library()
 
 @register.inclusion_tag('nav_menu.html', takes_context=True)
 def draw_menu(context: Context, menu: str) -> Dict[str, Any]:
-    # try:
-    #     menuId = Menu.objects.filter(slug=menu).first().pk
-    #     items = MenuItem.objects.filter(menu_id=menuId)
-    #     items_values = items.values()
-
-    #     root_items = [item for item in items_values.filter(parent=None)]
-  
-    #     selected_item_id = int(context['request'].GET[menu])
-
-    #     selected_item = items.get(id=selected_item_id)
-    #     selected_item_id_list = get_selected_item_id_list(selected_item, root_items, selected_item_id)
-
-     
-    #     for item in root_items:
-    #         if item['id'] in selected_item_id_list:
-    #             item['child_items'] = get_child_items(items_values, item['id'], selected_item_id_list)
-
-    #     result_dict = {'items': root_items}
-    #     print(" result_dict: ", result_dict)
+    """Пользовательский template tag для рендеринга древовидного меню"""
     try:
         items = MenuItem.objects.filter(menu__title=menu)
         items_values = items.values()
@@ -62,6 +44,7 @@ def draw_menu(context: Context, menu: str) -> Dict[str, Any]:
     
 
 def build_querystring(context: Context, menu: str) -> str:
+    """Формирование строки запроса на основе контекста запроса"""
     querystring_list = []
 
     for key in context['request'].GET:
@@ -75,6 +58,7 @@ def build_querystring(context: Context, menu: str) -> str:
 
 
 def get_child_items(items_values, current_item_id, selected_item_id_list):
+    """Возвращает список дочерних элементов для текущего id пункта меню"""
     item_list = [item for item in items_values.filter(parent_id=current_item_id)]
     for item in item_list:
         if item['id'] in selected_item_id_list:
@@ -83,6 +67,7 @@ def get_child_items(items_values, current_item_id, selected_item_id_list):
 
 
 def get_selected_item_id_list(parent: MenuItem, primary_item: List[MenuItem], selected_item_id: int) -> List[int]:
+    """Возвращает список id выбранных пунктов меню"""
     selected_item_id_list = []
 
     while parent:
